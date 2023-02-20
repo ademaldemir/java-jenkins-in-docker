3484d49b5211422a7bf4acfd62d4ee6508ef233a
Adem Aldemir <adem.aldemir@outlook.com>
2023-02-20 12:25:43 +0300

    Changes and deletes.

--- src/main/java/org/shreddedcoderx/Main.java
+++ src/main/java/org/shreddedcoderx/Main.java
@@ -2,10 +2,8 @@ package org.shreddedcoderx;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(checkIfInputIsAnEvenNumber(122));
+        System.out.println("Jenkins Demo.");
     }
 
-    public static boolean checkIfInputIsAnEvenNumber(int number) {
-        return number % 2 == 0;
-    }
+
 }
